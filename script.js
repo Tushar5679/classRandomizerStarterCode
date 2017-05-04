d3f327938a3afafc7df62ec00d566bd01cfa3331
Tushar Sharma <sharma@aclanyc.com>
2017-05-04 00:21:05 +0000

    add

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
+var ScriptEders=["Joel" ,"Alan" , "Tushar" ,"Yanella", "odalysis" , "Josue"];
+var ScriptEducators=["Aaron" ,"Justin" , "Zach" , "Julia" , "Alyxe"];
+$( "#studentButton" ).click(function() {
+    var randomstudents= Math.floor(Math.random()*ScriptEders.length);
+  $( "#studentDisplay" ).append(ScriptEders[randomstudents]);
+ 
+});
+$( "#teacherButton" ).click(function() {
+var random= Math.floor(Math.random()*ScriptEducators.length);
+ $( "#teacherDisplay" ).append(ScriptEducators[random]);
 
 
-
+});
 });
